c65e02cb20620217cd48b5f6214c7f8ce46a7bce
Jon Hamshaw <Jonathan_Hamshaw@mckinsey.com>
2015-04-09 16:13:56 +0100

    renaming chats to timer

--- www/js/app.js
+++ www/js/app.js
@@ -48,24 +48,24 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.timer', {
+      url: '/timer',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'tab-timer': {
+          templateUrl: 'templates/tab-timer.html',
+          controller: 'TimerCtrl'
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+    // .state('tab.chat-detail', {
+    //   url: '/chats/:chatId',
+    //   views: {
+    //     'tab-chats': {
+    //       templateUrl: 'templates/chat-detail.html',
+    //       controller: 'ChatDetailCtrl'
+    //     }
+    //   }
+    // })
 
   .state('tab.account', {
     url: '/account',
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,15 +15,8 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('TimerCtrl', function($scope) {
+  // todo
 })
 
 .controller('AccountCtrl', function($scope) {
